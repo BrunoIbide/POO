6a83d2bd50636cb3ae778f8852a6eb37a5379854
BrunoIbide <brunoibide@hotmail.com>
2020-03-06 22:06:55 -0300

    2

---
+++ PrimeiraAula2/CalculoCombustivel.java
@@ -0,0 +1,38 @@
+
+// Dado os valores do Alcool e da Gasolina, descubra qual é o melhor, sabendo que a gasolina é 70% mais performatica
+
+
+/** valor da gasolina * 0.7
+ * se menor que o Valor acool
+ * gasolina é melhor
+ * senao
+ * alcool é melhor
+ * 
+ */
+public class CalculoCombustivel
+{
+    public static void main(String[] args){
+        
+        double valorGsolina = 4.00;   
+        double valorAlcool  = 2.50;
+        double resposta = valorGsolina * 0.7;
+        
+        
+        if(resposta > valorAlcool){
+            System.out.printl("Gasolina é melhor");
+        
+        }
+        else{
+            
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
