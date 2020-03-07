192156870edabf2fbe88d2f3261ece1f5015d820
BrunoIbide <brunoibide@hotmail.com>
2020-03-06 21:50:34 -0300

    2

---
+++ PrimeiraAula2/Variaveis.java
@@ -0,0 +1,48 @@
+
+/**
+ * float >> double
+ * char( um caractere) String(igual o char no C++)
+ * int >>> int
+ * boolean === true ou false;
+ */
+
+
+
+
+
+
+
+
+
+public class Variaveis
+{
+   public static void main(String[]args){
+       
+     double valorSalario= 100.00; 
+     
+     int numeroDependentes = 5;
+     
+     String nomeCidade="Bauru";
+     
+     char letra='B';
+     
+     boolean temSaldo = true;
+     
+     double valor1 = 10.20;
+     double valor2 = 30.20;
+     double soma = valor1 + valor2;
+     
+     
+     System.out.println("A soma é:"+soma);
+     
+     if(soma > 100){
+         System.out.println("A soma é maior que 100");
+         
+        }
+        else{
+        System.out.println("A soma é menor que 100");
+        }
+      
+       
+    }
+}
