b985d5f05b5085b37c93ae81aa8b770e8f44fde2
BrunoIbide <brunoibide@hotmail.com>
2020-03-13 21:30:38 -0300

    P.O.O

---
+++ Aula2/Pessoa.java
@@ -0,0 +1,16 @@
+
+public class Pessoa
+{
+    //Atributos =  caracteristicas
+    String cpf;
+    String rg;
+    int idade;
+    String nome;
+    String endereco;
+    String email;
+    boolean doador;
+    
+   
+    
+    
+}
